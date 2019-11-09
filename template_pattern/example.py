ad20eee76a2484d57800715240953619c36da426
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2019-11-09 08:54:49 -0500

    Add template pattern example

--- template_pattern/example.py
+++ template_pattern/example.py
@@ -1,8 +1,75 @@
+"""
+Thoughts: A lot of using variables that's defined outside the class or the
+init function here.
+"""
+
+import sqlite3
+import datetime
+
+
 class QueryTemplate:
-	def connect(self):
-        pass
+    def connect(self):
+        self.conn = sqlite3.connect("sales.db")
 
     def construct_query(self):
-        pass
+        raise NotImplementedError("Implement before using")
+
+    def do_query(self):
+        results = self.conn.execute(self.query)
+        self.results = results.fetchall()
+
+    def format_results(self):
+        output = []
+        for row in self.results:
+            row = [str(i) for i in row]
+            output.append(", ".join(row))
+        self.formatted_results = "\n".join(output)
+
+    def output_results(self):
+        raise NotImplementedError("Implement before using")
+
+    def process_format(self):
+        self.connect()
+        self.construct_query()
+        self.do_query()
+        self.format_results()
+        self.output_results()
+
+
+class NewVehicleQuery(QueryTemplate):
+    def construct_query(self):
+        self.query = "SELECT * FROM Sales WHERE new='true'"
+
+    def output_results(self):
+        print(self.formatted_results)
+
+
+class UserGrossQuery(QueryTemplate):
+    def construct_query(self):
+        self.query = ("select salesperson, sum(amt) " +
+                      "from Sales group by salesperson")
+
+    def output_results(self):
+        filename = "gross_sales_{0}".format(
+            datetime.date.today().strftime("%Y%m%d")
+        )
+        with open(filename, 'w') as outfile:
+            outfile.write(self.formatted_results)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
