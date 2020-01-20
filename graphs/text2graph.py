fbce3a5946f351038b611d8f7a66699f655b304a
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2020-01-20 18:27:31 -0500

    update text2graph code

--- graphs/text2graph.py
+++ graphs/text2graph.py
@@ -86,14 +86,32 @@ class Text2Graph:
 
 
 if __name__ == "__main__":
-    from gslabs.db.db_requester import DBRequester
-
-    db_requester = DBRequester()
-    results, col_names = db_requester.get_query_results("""SELECT message 
-    FROM messages WHERE is_question=1 and message not like like "%{file:%" 
-    and message not like "%ttps%" """)
-
-    document = ". ".join(results)
+    document = """Kinematics is the branch of classical mechanics that describes the motion of
+points, objects and systems of groups of objects, without reference to the
+causes of motion (i.e., forces ). The study of kinematics is often referred
+to as the geometry of motion. Objects are in motion all around us. Everything
+from a tennis match to a space-probe flyby of the planet Neptune involves
+motion. When you are resting, your heart moves blood through your veins.
+Even in inanimate objects there is continuous motion in the vibrations of
+atoms and molecules. Interesting questions about motion can arise: how long
+will it take for a space probe to travel to Mars? Where will a football land
+if thrown at a certain angle? An understanding of motion, however, is also key
+to understanding other concepts in physics. An understanding of acceleration,
+for example, is crucial to the study of force. To describe motion,
+kinematics studies the trajectories of points, lines and other geometric
+objects, as well as their differential properties (such as velocity and
+acceleration). Kinematics is used in astrophysics to describe the motion of
+celestial bodies and systems; and in mechanical engineering, robotics and
+biomechanics to describe the motion of systems composed of joined parts
+(such as an engine, a robotic arm, or the skeleton of the human body). A
+formal study of physics begins with kinematics. The word kinematics comes
+from a Greek word kinesis meaning motion, and is related to other English
+words such as cinema (movies) and kinesiology (the study of human motion).
+Kinematic analysis is the process of measuring the kinematic quantities
+used to describe motion. The study of kinematics can be abstracted into
+purely mathematical expressions, which can be used to calculate various
+aspects of motion such as velocity, acceleration, displacement, time,
+and trajectory."""
 
     doc = Text2Graph(document)
     doc.preprocess()
