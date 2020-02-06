729df2ddefd1f14754cd591a639a6490810e97ba
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2020-02-06 06:44:18 -0500

    Add permutation function

--- combinatorics/explore.py
+++ combinatorics/explore.py
@@ -1,10 +1,38 @@
-def permutations(n, r):
-    elements = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-                'Y', 'Z']
-    if n < len(elements):
-        elements = elements[:n]
-    else:
-        ValueError("Choose n less than 27")
+from utils import LETTERS
 
-    sample_space = set({})
+
+def n_permute_r(n, r):
+    elements = LETTERS[:n]
+    perm_evol_dict = {0: elements}
+
+    def two_list_permutation(l1, l2):
+        permutations = []
+
+        for e1 in l1:
+            for e2 in l2:
+                if (e1 not in e2) and (e2 not in e1):
+                    permutations.append(e1 + e2)
+        return permutations
+
+    print('n: {0}, '
+          'r: {1}, '
+          'permutations: {2}, '
+          'set: {3}'.format(n, 1, len(perm_evol_dict[0]), perm_evol_dict[0]))
+
+    for i in range(1, r):
+        perm_evol_dict[i] = \
+            two_list_permutation(perm_evol_dict[i - 1], elements)
+
+        print('n: {0}, '
+              'r: {1}, '
+              'permutations: {2}, '
+              'set: {3}'.format(n, i + 1, len(perm_evol_dict[i]),
+                                perm_evol_dict[i]))
+
+
+if __name__ == "__main__":
+    for i in range(3, 7):
+        print("\n{0} element permutation".format(i))
+        print("=====================")
+
+        n_permute_r(i, i)
